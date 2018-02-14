efd1f6438891587a9a0648a9f9606610891f22d3
q0j0p <craicster@gmail.com>
2018-02-13 22:11:12 -0500

    wip for one-indexed swc files

--- analyze_swcfile.py
+++ analyze_swcfile.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import collections
 
+"""These functions use zero-indexing (arrays always start at zero), regardless of the .swc index"""
 def preview_file(swcfile, rows=10):
     """Preview swc header and first few lines"""
     with open(swcfile, 'r') as f:
@@ -16,7 +17,7 @@ def load_to_dataframe(swcfile, skiprows=1, sep=' '):
                 )
     return n_df
 
-def euc_distance_to_root(n_df, root=0):
+def euc_distance_to_root(n_df, root=0): # root=1 for "one-index"
     """For each point, calculate euclidean distance to root (assume index=0);
     add as column euc_dist_root"""
     dist_coords = np.asarray(
@@ -43,8 +44,12 @@ def get_child_list(n_df):
 
 def get_termini(n_df):
     child_list = get_child_list(n_df)
-    termini = collections.orderedlist(set(n_df.index.values) - set(child_list))
+    termini = list(set(n_df.index.values[1:]) - set(child_list.keys()))
     return termini
+
+def get_branches(n_df): 
+    child_list = get_child_list(n_df) 
+    return [a for a in child_list if len(child_list[a]) > 1]
 
 def get_path_to_root(n, path_to_root, n_df):
     """Given a point, return a list of points connecting to the root"""
@@ -52,10 +57,12 @@ def get_path_to_root(n, path_to_root, n_df):
     if n != -1:
         path_to_root.append(n)
 #        print n
-        get_path_to_root(n, path_to_root, n_df)
+#        get_path_to_root(n, path_to_root, n_df)
+
     return path_to_root
 
 def get_dist_to_root(n, n_df):
     """Given a point n, get distance to root using get_path_to_root"""
     path_array = np.asarray(get_path_to_root(n,[],n_df))
-    return distance_to_parent(n_df)[path_array[:-1]].sum()
+    print(path_array)
+    #return distance_to_parent(n_df)[path_array[:-1]].sum()
