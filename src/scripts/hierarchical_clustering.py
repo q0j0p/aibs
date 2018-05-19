a939f11db4f06024e9ffa33a0e04567bb5ff38a6
q0j0p <emailjamesc@gmail.com>
2018-05-18 23:11:58 -0700

    scripts used for current expt-- need to integrate into main code

--- src/scripts/hierarchical_clustering.py
+++ src/scripts/hierarchical_clustering.py
@@ -3,15 +3,17 @@ from csv import reader
 import numpy as np
 import h5py
 import zipfile
+import pymongo
 from scipy.cluster import hierarchy
 import matplotlib.pyplot as plt
+from PIL import Image
+import requests
 sys.path.append("..")
 import src
 from src.utils import expts
 from src.utils import zipfiles
 from src import swcfunctions
 from src import neuroncollector
-
 
 
 class TheExpt(expts.Expt):
@@ -44,19 +46,19 @@ class TheExpt(expts.Expt):
             swcfile_list.append((a, zipfiles.get_zipped_data(f"{src.PROJECT_ROOT}/raw/data/swc_data/{a}", string="CNG version")))
 
         # neuron_name : swc_filename
-        neuron_dict = {}
+        self.neuron_dict = {}
         for a in self.neuron_list:
             for b in list(filter(lambda x: a in "".join(x[1]), swcfile_list)):
-                neuron_dict[a] = b[0]
+                self.neuron_dict[a] = b[0]
 
-        swc_gen = self.generate_swc_file_buffer(neuron_dict)
+        swc_gen = self.generate_swc_file_buffer()
         return swc_gen
 
-    def generate_swc_file_buffer(self, neuron_dict):
+    def generate_swc_file_buffer(self):
         """Find the appropriate file corresponding to neuron_name in neuron_dict.keys() from .zip files"""
-        for a in neuron_dict:
-                if neuron_dict[a]:
-                    archive = zipfile.ZipFile(src.PROJECT_ROOT+"/raw/data/swc_data/"+neuron_dict[a])
+        for a in self.neuron_dict:
+                if self.neuron_dict[a]:
+                    archive = zipfile.ZipFile(src.PROJECT_ROOT+"/raw/data/swc_data/"+self.neuron_dict[a])
                     for b in filter(lambda x: ("CNG version" in x.filename) and (a in x.filename),  archive.filelist):
                         yield (b.filename,archive.read(b))
                 else:
@@ -77,7 +79,7 @@ class TheExpt(expts.Expt):
                 with h5py.File(self.h5f, "a") as h5f:
                     h5f.create_dataset(f"{cellname}/pbcode", data=data)
             except Exception as e:
-                print(f"Error in {a[0]}\n\t{e}")
+                print(f"Error in {cellname}/pbcode\n\t{e}")
                 files_w_errors.append(a[0])
         self.error_files.append(files_w_errors)
 
@@ -92,11 +94,27 @@ class TheExpt(expts.Expt):
                 except Exception as e:
                     print(e)
 
+    def plot_2d_pbimage(self, neuron_name):
+        client = pymongo.MongoClient(host="mongodb://localhost/27017/")
+        db = client.aibs
+        url = db.nmorpho.find_one({"neuron_name":neuron_name})['png_url']
+        with h5py.File(self.h5f, "r") as h5f:
+            pbimage = h5f[f"{neuron_id}"].value
+        fig, ax = plt.subplots(1,2,figsize=(20,10))
+        ax[0].imshow(Image.open(requests.get(png_urls[name], stream=True).raw))
+        ax[0].set_title(neuron_name)
+        ax[1].imshow(np.rot90(pbimage.reshape(400,-1)), cmap=plt.cm.RdYlBu_r, extent=[0, 400, 0, 400])
+        ax[1].set_xlim([0, 400])
+        ax[1].set_ylim([0, 400])
+        plt.show()
+
 
 # Standalone script for hierchical clustering.  Can integrate l8r.
 from scipy.cluster.hierarchy import dendrogram, linkage
 
 def render_data(self):
+    """Extract image as array and celltypes"""
+
     with h5py.File(self.h5f,"a") as h5f:
         X = np.zeros((len(h5f.keys()),160000))
         names = []
@@ -104,15 +122,30 @@ def render_data(self):
             X[i] = h5f[f"{a}/pbimage"].value.reshape(160000)
             names.append(a)
         neuron_dict = neuroncollector.ExtractData("drosophila melanogaster").get_project_info()
-        celltypes = [neuron_dict[a][-1] for a in names]
+        celltypes = [(a,neuron_dict[a][-1]) for a in names]
     return X, celltypes
 
 def plot_dendrogram(X,labels,**kwargs):
     Z = hierarchy.linkage(X, **kwargs)
-    fig, ax = plt.subplots(1,1,figsize=(20,60))
+    fig, ax = plt.subplots(1,1,figsize=(20,360))
     dendrogram = hierarchy.dendrogram(Z, ax=ax,labels=labels,leaf_font_size=16,
                             above_threshold_color='#bcbddc',
                             orientation='right')
+    plt.show()
+    return Z
+
+def plot_2d_pbimage(self, neuron_name):
+    client = pymongo.MongoClient(host="mongodb://localhost/27017/")
+    db = client.aibs
+    url = db.nmorpho.find_one({"neuron_name":neuron_name})['png_url']
+    with h5py.File(self.h5f, "r") as h5f:
+        pbimage = h5f[f"{neuron_name}/pbimage"].value
+    fig, ax = plt.subplots(1,2,figsize=(10,6))
+    ax[0].imshow(Image.open(requests.get(url, stream=True).raw))
+    ax[0].set_title(neuron_name)
+    ax[1].imshow(np.rot90(pbimage.reshape(400,-1)), cmap=plt.cm.RdYlBu_r, extent=[0, 400, 0, 400])
+    ax[1].set_xlim([0, 400])
+    ax[1].set_ylim([0, 400])
     plt.show()
 
 if __name__ == "__main__":
